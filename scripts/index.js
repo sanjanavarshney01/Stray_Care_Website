b50d35e8d965508fe0b5a17def2adc2df7ffd886
sanjanavarshney01 <sanjana.varshney_cs19@gla.ac.in>
2022-05-24 14:27:17 +0530

    commit changes

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,8 @@ document.getElementById("mybtn1").onclick = function() {
 document.getElementById("mybtn2").onclick = function() {
     location.href = "https://drive.google.com/file/d/1TJIsYtsbIAkM5VqyqMezJDxN0TlcuV7b/view?usp=sharing";
 }
+
+function showMessage() {
+    alert("This version of application will be available soon.");
+}
+

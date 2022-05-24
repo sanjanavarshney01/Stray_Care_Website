7780544d906d77f66bf88f77fdba073fef37f36a
sanjanavarshney01 <sanjana.varshney_cs19@gla.ac.in>
2022-05-24 14:20:22 +0530

    added event listener

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+document.getElementById("mybtn1").onclick = function() {
+    location.href = "https://drive.google.com/file/d/1TJIsYtsbIAkM5VqyqMezJDxN0TlcuV7b/view?usp=sharing";
+};
+
+document.getElementById("mybtn2").onclick = function() {
+    location.href = "https://drive.google.com/file/d/1TJIsYtsbIAkM5VqyqMezJDxN0TlcuV7b/view?usp=sharing";
+}
